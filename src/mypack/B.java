5a3812a56f822cffabb5c4835505c339db94cc63
VoHoangDuong <duong6046237@gmail.com>
2021-04-06 22:22:43 +0700

    update

---
+++ src/mypack/B.java
@@ -0,0 +1,8 @@
+package mypack;
+import pack.*;
+public class B {
+    public static void main(String[] args) {
+        A Obj = new A();//Compile Time Error
+        Obj.msg();//Compile Time Error
+    }
+}
