393e7e59456d15d1c8f45d23f9a78cc6d5926824
VoHoangDuong <duong6046237@gmail.com>
2021-04-06 22:30:12 +0700

    update

---
+++ src/mypack/D.java
@@ -0,0 +1,10 @@
+package mypack;
+
+import pack.C;
+
+public class D {
+    public static void main(String[] args) {
+        C Obj = new C();
+        Obj.getInformation();
+    }
+}
---
+++ src/pack/C.java
@@ -0,0 +1,7 @@
+package pack;
+
+public class C {
+    public void getInformation(){
+        System.out.println("Name, Age, Address");
+    }
+}
